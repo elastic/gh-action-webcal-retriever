5135393b0211b86320366177398d63dcb65d7c79
Alexander Wert <alexander.wert@elastic.co>
2023-02-17 17:20:11 +0100

    added webcal schema replacement

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const core = require('@actions/core')
 const ical = require('node-ical');
 
 const main = async () => {
-  const url = core.getInput('webcal_url')
+  const url = core.getInput('webcal_url').replace('webcal', 'https');
   const webEvents = await ical.async.fromURL(url);
   const person_id = Object.entries(webEvents)
     .map(([key, event]) => event)
@@ -15,10 +15,10 @@ const main = async () => {
       return startDate <= now && now <= endDate;
     })?.attendee;
   if (!person_id) {
-    throw Error('Could not find attendee for today!')
+    throw Error('Could not find attendee for today!');
   }
 
-  core.setOutput('person_id', person_id)
+  core.setOutput('person_id', person_id);
 }
 
 main().catch(err => core.setFailed(err.message))
