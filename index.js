4f4a3433857033a1544b449386aacf578b60ac5f
Alexander Wert <alexander.wert@elastic.co>
2023-02-18 10:06:34 +0100

    Added days offset

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const core = require('@actions/core')
 const ical = require('node-ical');
 
 const main = async () => {
+  const days_offset_str = core.getInput('days_offset') ?? 0;
+  const days_offset = days_offset_str ? parseInt(days_offset_str) : 0;
   const url = core.getInput('webcal_url').replace('webcal', 'https');
   const webEvents = await ical.async.fromURL(url);
   const person_id = Object.entries(webEvents)
@@ -11,8 +13,10 @@ const main = async () => {
     .find((event) => {
       const startDate = Date.parse(event.start);
       const endDate = Date.parse(event.end);
-      const now = Date.now();
-      return startDate <= now && now <= endDate;
+      
+      const targetDate = Date.now();
+      targetDate.setDate(targetDate.getDate() + days_offset);
+      return startDate <= targetDate && targetDate <= endDate;
     })?.attendee;
   if (!person_id) {
     throw Error('Could not find attendee for today!');
